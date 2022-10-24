a40d76409265b8d311a0683146301d0f65c32316
seminyoon1 <seminyoon1@gmail.com>
2022-10-24 15:55:06 -0400

    Added some backend and left work for front end devs
    Plz fix css

---
+++ data.py
@@ -0,0 +1,13 @@
+def getData():
+    arr = {}
+    data = open("data.txt", "r")
+    lines = data.readlines()
+
+    for textLine in lines:
+        word = textLine.split(",")
+        arr[word[0][0:len(word[0])]] = word[1][0:len(word[1])-1]
+        #return data
+    data.close()
+    return arr
+print(getData())
+
--- web.py
+++ web.py
@@ -1,24 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
-  
+from data import getData
+import random
+
 app = Flask(__name__)
 arr = []
+arrList = getData()
+
 @app.route('/')
 def index():
+    itemss = random.choice(list(arrList.items()))
     #basic rendering, must be in the templates folder
-   return render_template("home.html")
+    return render_template("home.html", arrItem = itemss[0], arrNum = itemss[1])
  
 #app.route("variable name") correlates to the function, called from html button form action
 @app.route('/text', methods=['GET', 'POST'])
 def text():
+    itemss = random.choice(list(arrList.items()))
     if request.method == "GET":
-        return render_template("home.html", comments=arr)   
+        return render_template("home.html", arrItem = itemss[0], arrNum = itemss[1], comments=arr)
     arr.append(request.form["text_input"])
     return redirect(url_for('text'))
 
 @app.route('/reset', methods=['GET', 'POST'])
 def reset():
+    itemss = random.choice(list(arrList.items()))
     if request.method == "GET":
-        return render_template("home.html")
+        return render_template("home.html", arrItem = itemss[0], arrNum = itemss[1])
     arr.clear()
     return redirect(url_for('reset'))
 if __name__ == '__main__':
